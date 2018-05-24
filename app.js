81fef5651bf6fe323c472a99d235f40209308439
Rajeshwaran <rajeshfederer2@gmail.com>
2018-05-24 10:40:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+let action = require('./lib/action');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  console.log('ACTION NAME ', req.body.result.action);
+  return action[req.body.result.action](req, res);
+});
+
+app.listen(port, function () {
+  console.log('AGENT is running my app on  PORT: ' + port);
+});
