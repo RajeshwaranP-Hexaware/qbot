d03d7a26ce08ecc6c825187b93765a1546a302cd
Rajeshwaran <rajeshfederer2@gmail.com>
2018-05-24 11:32:00 +0530

    v2 changes

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser');
 let action = require('./lib/action');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
   console.log('ACTION NAME ', req.body.result.action);
-  return action[req.body.result.action](req, res);
+  return action[req.body.queryResult.action](req, res);
 });
 
 app.listen(port, function () {
