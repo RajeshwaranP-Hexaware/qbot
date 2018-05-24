5ad524112b2c993c1bcae0f1107b916fd2e12512
Rajeshwaran <rajeshfederer2@gmail.com>
2018-05-24 11:34:44 +0530

    v2 changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  console.log('ACTION NAME ', req.body.result.action);
+  console.log('ACTION NAME ', req.body.queryResult.action);
   return action[req.body.queryResult.action](req, res);
 });
 
